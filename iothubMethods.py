3237f7cf1926739c4709eca2bcc8862d40ea4c51
danielcarlson85 <info@danielcarlson.net>
2023-11-08 15:21:49 +0100

    Update iothubMethods.py

--- iothubMethods.py
+++ iothubMethods.py
@@ -4,19 +4,6 @@ import utils
 import time
 import json
 import excersiceAlgorithms
-
-def reboot_rpi():
-    print("reboot")
-    utils.sense.show_letter("R", text_colour= utils.setredtext())
-    subprocess.run(["sudo","reboot"])
-
-
-def shutdown_rpi():
-    print("shutting down...")
-    utils.sense.show_letter("S", text_colour= utils.setredtext())
-    time.sleep(2)
-    utils.setBlackColor()
-    subprocess.run(["sudo","poweroff"])
     
 def startnow(usertext):
     print(f"Method started startnow")
@@ -40,7 +27,6 @@ def startnow(usertext):
     start.UserData.data = json.loads(clean_object)
     start.UserData.data['TrainingData'] = []
                     
-    start.UserData.startExcersice = True        
     excersiceAlgorithms.checkForDeviceMovements()
     
     print("Startnow method exited")
@@ -51,15 +37,3 @@ def startnow(usertext):
 #    finally:
 #        utils.restart_bound_script()
 #        return-1
-        
-def loggin_on_device():
-    if not (hasLoggedInOnDevice):
-        utils.setGreenColor()
-        start.UserData.hasLoggedInOnDevice=True
-    
-def loggin_out_device():
-    if (hasLoggedInOnDevice):
-        utils.setBlackColor()
-        start.UserData.hasLoggedInOnDevice=False
-    
-
